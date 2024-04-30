25d0da2049c432e7a1c244c60192b5ea1f2adf8b
ben-sanati <bensanatiwork@gmail.com>
2024-04-30 23:20:44 +0100

    Added Data

---
+++ test.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+if __name__ == "__main__":
+    db_file = "data/gold_std/data.db"
+
+    conn = sqlite3.connect(db_file)
+    sql_query = """SELECT name FROM sqlite_master 
+    WHERE type='table';"""
+ 
+    # Creating cursor object using connection object
+    cursor = conn.cursor()
+
+    # executing our sql query
+    cursor.execute(sql_query)
+    print("List of tables\n")
+
+    # printing all tables list
+    tables = cursor.fetchall()
+    for table in tables:
+        table = table[0]
+        cursor.execute(f"SELECT * FROM {table}")
+        print(f"Table {table}: {[description[0] for description in cursor.description]}", flush=True)
