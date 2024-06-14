d7787ac3bb1f3614ec74ca23a564a0c044539931
ben-sanati <bensanatiwork@gmail.com>
2024-06-14 15:12:45 +0100

    Improved Code Modularity

--- src/map_alignment/bbox_comparison.py
+++ src/map_alignment/bbox_comparison.py
@@ -2,42 +2,29 @@ import argparse
 import os
 import pickle
 import sys
+import random
+import numpy as np
+import open3d as o3d
+from scipy.spatial import KDTree
 
 sys.path.insert(0, r"../..")
 
 from src.utils.config import ConfigLoader  # noqa
+from src.utils.transformations import Transforms  # noqa
+from src.utils.visualisation import Visualiser  # noqa
 
 
 class BBoxComparison:
-    def __init__(self, base_data, comparison_data):
+    def __init__(self, base_data, comparison_data, base_mesh, comparison_mesh):
         self.base_data = base_data
         self.comparison_data = comparison_data
+        self.base_mesh = base_mesh
+        self.comparison_mesh = comparison_mesh
+        self.matches = {}
+
+        # Instance util classes
+        self.visualiser = Visualiser()
+        self.transforms = Transforms()
 
     def match_bboxes(self):
         print(f"Base: {self.base_data}\n\nComparison: {self.comparison_data}")
-
-
-if __name__ == "__main__":
-    # Setup argparse config
-    parser = argparse.ArgumentParser(description="Processing Configuration")
-    parser.add_argument(
-        "--data", type=str, help="Data Folder Name.", default="ideal_scan"
-    )
-    args = parser.parse_args()
-    data_folder = args.data
-
-    if data_folder == "gold_std":
-        raise ValueError("The parameter 'gold_std' is not allowed for --data.")
-
-    # Load the configuration
-    os.chdir("../..")
-    config_path = r"src/common/configs/variables.cfg"
-    cfg = ConfigLoader(config_path, data_folder)
-
-    with open(cfg.pickle_path, "rb") as read_file:
-        comparison_variables = pickle.load(read_file)
-
-    print(comparison_variables)
-
-    base_bboxes = comparison_variables["base_bboxes"]
-    comparison_bboxes = comparison_variables["comparison_bboxes"]
